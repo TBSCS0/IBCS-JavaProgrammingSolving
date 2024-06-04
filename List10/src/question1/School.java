d5604404d8a48b34c9a2a1605a3f36335318d3cc
oscarneiva <oscarneivaeneto@gmail.com>
2024-06-04 10:13:15 -0300

    list 10 q1

--- List10/src/question1/School.java
+++ List10/src/question1/School.java
@@ -3,10 +3,14 @@ package question1;
 public class School {
     public static void main(String[] args) {
         Student std1 =  new Student("Yenah", "yenah2020", "123", "yenah@email.com", "Gangnam");
-        Student std2 =  new Student("Yenah", "yenah2020", "123", "yenah@email.com", "Gangnam");
+        Student std2 =  new Student("Caio", "caio2020", "123", "caio@email.com", "Porto");
         Student std3 =  new Student("Ed", "ed2020", "123", "eduardo@email.com", "São Paulo");
 
-        std1.setName("caio");
-        System.out.println(std2.getName());
+        System.out.println(std1.getName());
+        std3.setName("Eduardo");
+
+        Student std4 = new Student("Nikhil", null, null, null, null);
+
+        System.out.println(std4.getAddress());
     }
 }
--- List10/src/question1/Student.java
+++ List10/src/question1/Student.java
@@ -4,14 +4,25 @@ public class Student extends User{
     private String email;
     private String address;
 
-
-    public Student(String name, String username, String password) {
-        super(name, username, password);
-    }
-
     public Student(String name, String username, String password, String email, String address) {
         super(name, username, password);
         this.email = email;
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
         this.address = address;
     }
 }
