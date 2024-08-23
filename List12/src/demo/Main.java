de94fa61904c5353b3d86e431407e06d2ab1940b
oscarneiva <oscarneivaeneto@gmail.com>
2024-08-23 13:20:04 -0300

    selection sort

--- List12/src/demo/Main.java
+++ List12/src/demo/Main.java
@@ -4,6 +4,26 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        selectionSort();
+    }
+
+    public static void selectionSort(){
+        int[] arr = {1618, 1, 3, 2, 314, 32, 42, 757};
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            int min_index = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[min_index] > arr[j]) {
+                    min_index = j;
+                }
+            }
+            int aux = arr[i];
+            arr[i] = arr[min_index];
+            arr[min_index] = aux;
+        }
+    }
+
+    public static void bubbleSort(){
         int[] arr = {1618, 1, 3, 2, 314, 32, 42, 757};
         boolean swapped;
         int n = arr.length;
